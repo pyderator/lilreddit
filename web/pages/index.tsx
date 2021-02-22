bef1135edacbd30ddefadde43913116fd606c97a
pyderator <oscarmike286@gmail.com>
2021-02-22 11:43:11 +0545

    Added tailwindcss & configured it

---
+++ web/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Lil Reddit</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+     
+      <div className='bg-black'>
+        <h1 className='text-purple-900 text-4xl font-bold'>Taidwind YIPEEEEE!!!!</h1>
+      </div>
+      
+      
+    </div>
+  )
+}
